120e7008f2a76e0020c16725c7036ed286e72252
MS <ashokaagency2025@gmail.com>
2026-07-26 00:18:33 +0530

    Remove customer id from order launch URL

--- src/components/CustomerMobileLayout.tsx
+++ src/components/CustomerMobileLayout.tsx
@@ -89,7 +89,6 @@ const CustomerMobileLayout = () => {
     const orderUrl = new URL(ORDER_APP_URL);
     orderUrl.searchParams.set('name', customerHeading);
     orderUrl.searchParams.set('role', 'customer');
-    orderUrl.searchParams.set('customerId', portalData.customer?.id || customerHeading);
     orderUrl.searchParams.set('area', portalData.customer?.area || '');
     orderUrl.searchParams.set('returnUrl', window.location.href);
     window.open(orderUrl.toString(), '_blank', 'noopener,noreferrer');
